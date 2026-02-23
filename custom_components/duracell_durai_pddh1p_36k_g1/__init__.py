6ccb9079398727db2b4a88b6d5d509beb124fc6d
tonymul <tony_mul@live.co.uk>
2026-02-23 23:28:07 +0000

    Add files via upload

--- custom_components/duracell_durai_pddh1p_36k_g1/__init__.py
+++ custom_components/duracell_durai_pddh1p_36k_g1/__init__.py
@@ -7,8 +7,9 @@ async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Updated for current HA versions
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
